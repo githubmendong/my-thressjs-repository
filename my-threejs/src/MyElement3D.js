9d677053eb0940e4c8e386f92cda9b3cc9e1e9d2
shin-dongheon <120146699+githubmendong@users.noreply.github.com>
2023-11-27 15:39:35 +0900

    # 02

--- my-threejs/src/MyElement3D.js
+++ my-threejs/src/MyElement3D.js
@@ -1,7 +1,20 @@
+import {useRef} from "react";
+import {useFrame} from "@react-three/fiber";
+
 function MyElement3D(){
+
+    const refMesh = useRef()
+    useFrame((state, delta) =>{
+        refMesh.current.rotation.y += delta
+    })
     return(
         <>
-            <mesh>
+            <directionalLight position={[1,1,1]}/>
+            <mesh ref={refMesh} rotation-x={45*Math.PI/180} rotation-y={45*Math.PI/180}>
+                {/*x 축으로 0도*/}
+                {/*y 축으로 45도*/}
+                {/*r3f는 각도를 라디안이라는 단위로 지정해야한다.*/}
+
                 <boxGeometry/>
                 <meshStandardMaterial color="#e67e22"/>
             </mesh>
