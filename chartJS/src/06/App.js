2f2f32a9d098093a8e1d284e47303bd3ff59d172
shin-dongheon <120146699+githubmendong@users.noreply.github.com>
2023-11-29 18:58:07 +0900

    #

--- chartJS/src/06/App.js
+++ chartJS/src/06/App.js
@@ -2,6 +2,26 @@ import React, {useRef} from "react";
 import {OrbitControls, Cylinder, Box, PositionalAudio} from "@react-three/drei";
 import {Canvas, useFrame} from "@react-three/fiber";
 
+
+function BoxComponent_1({ position }) {
+    const meshRef = useRef();
+    useFrame(({ clock }) => {
+        // if (meshRef.current) {
+        //     meshRef.current.rotation.y = clock.elapsedTime;
+        // }
+    });
+
+    return (
+        <mesh
+            // rotateY={}
+
+            ref={meshRef} position={position} scale={0.1} rotation={[Math.PI / 1, Math.PI / 2, 0]}>
+            <boxGeometry args={[30, 30, 80]} />
+            <meshStandardMaterial color={"rgb(255,136,136)"} />
+        </mesh>
+    );
+}
+
 function CylinderComponent({position}) {
     const meshRef = useRef();
     useFrame(({clock}) => {
@@ -16,7 +36,7 @@ function CylinderComponent({position}) {
             scale={0.1}
             rotation={[Math.PI / 2, 0, 0]}
             position={position}
-            args={[5, 5, 30, 12, 12, false, Math.PI * 0.25, Math.PI * 2.00]}
+            args={[5, 5, 30, 8, 12, false, Math.PI * 0.25, Math.PI * 2.00]}
         >
             <meshPhongMaterial color={"rgb(203,203,203)"}/>
         </Cylinder>
@@ -24,11 +44,16 @@ function CylinderComponent({position}) {
 }
 
 function MyElement3D() {
-    const cylinderProps = Array(10).fill().map((_, i) => [0.45 + i, 0, 1.5]);
+    const cylinderProps = Array(17).fill().map((_, i) => [0.45 + i, 0, 1.5]);
 
     return (
         <div id={"canvas-container"}>
-            <Canvas style={{display: 'block', width: '998px', height: '100vh', touchAction: 'none'}}>
+            <Canvas
+                camera={{
+                    fov:75,
+                    position:[25, 10, 15]
+                }}
+                style={{display: 'block', width: '998px', height: '100vh', touchAction: 'none'}}>
                 <OrbitControls/>
 
                 <axesHelper scale={100}/>
@@ -38,6 +63,10 @@ function MyElement3D() {
                 {cylinderProps.map((position, i) => (
                     <CylinderComponent key={i} position={position} />
                 ))}
+
+                <BoxComponent_1 position={[-4, 1.5, 1.5]}/>
+
+
             </Canvas>
         </div>
     )
